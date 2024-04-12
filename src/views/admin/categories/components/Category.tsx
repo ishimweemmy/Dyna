a90e7a327ec01aa791d69a514dea30c05f89565b
ishimweemmy <ishimweemmy24@gmail.com>
2024-04-12 17:46:01 +0200

    [feat]: added categories section

--- src/views/admin/categories/components/Category.tsx
+++ src/views/admin/categories/components/Category.tsx
@@ -3,10 +3,10 @@ import Card from "src/components/card";
 
 const Category:FC<TCategory> = ({ name, description, subCategories }) => {
   return (
-    <Card extra="w-full p-3 gap-4 ring-1 cursor-pointer">
+    <Card extra="w-full p-5 gap-4 ring-1 cursor-pointer">
         <div className="flex flex-col items-start justify-center gap-2">
             <span className="text-lg font-semibold">{name}</span>
-            <span className="text-lg font-semibold">{description}</span>
+            <span className="text-base font-semibold text-gray-700">{description}</span>
         </div>
         <div className="flex flex-col items-start justify-center gap-3">
             <span className="text-sm font-semibold underline">Subcategories</span>
